5c7244c910855cc1c44b4d625c9c573fe00e72f5
the_paper__ <the.paper@live.co.kr>
2021-02-14 23:34:06 +0900

    내장 랜덤함수 설명 추가

--- Assets/SimpleRandom/Scripts/SystemRandom.cs
+++ Assets/SimpleRandom/Scripts/SystemRandom.cs
@@ -7,6 +7,7 @@ namespace SimpleRandom
     public class SystemRandom : Random
     {
         //Donald E. Knuth's subtractive random number generator 쓴다고 함.
+        //"The Art of Computer Programming, volume 2: Seminumerical Algorithms" 이 책을 참고하라고함.
         //ref: https://rosettacode.org/wiki/Subtractive_generator
 
         System.Random m_Random = new System.Random();
