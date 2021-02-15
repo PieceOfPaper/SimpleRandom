60e3edde44e30f9b4a61aac0a6ed1818a774983c
이성희 <>
2021-02-15 17:31:40 +0900

    LCG 코드 개선

--- Assets/SimpleRandom/Scripts/LCGRandom.cs
+++ Assets/SimpleRandom/Scripts/LCGRandom.cs
@@ -6,23 +6,28 @@ namespace SimpleRandom
     /// </summary>
     public class LCGRandom : Random32
     {
+        //ref: https://ko.wikipedia.org/wiki/%EC%84%A0%ED%98%95_%ED%95%A9%EB%8F%99_%EC%83%9D%EC%84%B1%EA%B8%B0
         //ref: https://en.wikipedia.org/wiki/Linear_congruential_generator
 
-        protected long m_CurrentValue = 0;
-        protected virtual long A => 1664525;
-        protected virtual long C => 1013904223;
+
+        // 1. C와 M이 서로소여야 한다.
+        // 2. A-1이 M의 모든 소인수로 나눠져야 한다.
+        // 3.M이 4의 배수일 경우, A-1도 4의 배수여야 한다.
+        protected virtual uint A => 1664525;
+        protected virtual uint C => 1013904223;
+        protected virtual uint M => uint.MaxValue;
+
+
+        protected uint m_Seed = 0;
+
+
         public override int RandMax => int.MaxValue;
-
-
-        public override void Init(int seed)
-        {
-            m_CurrentValue = seed % RandMax;
-        }
-
+        public override void Init(int seed) => Init((uint)seed);
+        public override void Init(uint seed) => m_Seed = seed;
         public override int Rand()
         {
-            m_CurrentValue = (A * m_CurrentValue + C) % RandMax;
-            return (int)m_CurrentValue;
+            m_Seed = (A * m_Seed + C) % M;
+            return (int)(m_Seed >> 1);
         }
     }
 }
